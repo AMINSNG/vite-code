ed765006705bcaf02fd0ad0073210f59714b7a99
AMINSNG <aminsng83@gmail.com>
2025-11-23 20:40:56 +0330

    t3 day 4

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './styles/global.css';
-import App from './app/App.jsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles/global.css";
+import App from "./app/App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router";
 
-createRoot(document.getElementById('root')).render(
+const roots = createBrowserRouter([{path:'home',element:<App/>}]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={roots}>
+
+
+    </RouterProvider>
+  </StrictMode>
 );
